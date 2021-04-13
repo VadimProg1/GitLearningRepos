04601c3435b0a061bc206ac308f584d563bc0bac
Вадим Бабиков <b.vadim.alexeyevich@gmail.com>
2021-04-13 16:45:08 +0700

    добавил переменную b в форме

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -13,6 +13,7 @@ namespace WindowsFormsApp1
     public partial class Form1 : Form
     {
         int a = 1;
+        int b = 2;
         public Form1()
         {
             InitializeComponent();
